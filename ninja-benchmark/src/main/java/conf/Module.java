e4b18741211eeff02341f8188ee6a5983194b21b
Joe Lauer <joe@lauer.bz>
2016-02-23 18:07:51 -0500

    Trimmed benchmark size; disabled ehcache

---
+++ ninja-benchmark/src/main/java/conf/Module.java
@@ -0,0 +1,18 @@
+package conf;
+
+import com.google.inject.AbstractModule;
+import com.google.inject.Singleton;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+@Singleton
+public class Module extends AbstractModule {
+    static private final Logger log = LoggerFactory.getLogger(Module.class);
+    
+    @Override
+    protected void configure() {
+        // disable annoying ehcache update checks
+        System.setProperty("net.sf.ehcache.skipUpdateCheck", "true");
+    }
+
+}
